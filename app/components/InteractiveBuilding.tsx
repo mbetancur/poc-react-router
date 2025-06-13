aae348cabe85b689beaeff8dc853ae1b6386bd26
MateoB <slash-252@hotmail.com>
2025-06-13 17:29:57 -0500

    adding konva transform

--- app/components/InteractiveBuilding.tsx
+++ app/components/InteractiveBuilding.tsx
@@ -1,3 +1,4 @@
+import Konva from "konva";
 import { useRef, useState } from "react";
 import { Image, Label, Layer, Line, Rect, Text, Transformer } from "react-konva";
 import useImage from "use-image";
@@ -5,9 +6,7 @@ import useImage from "use-image";
 const ImageWithHover = ({ src = './PDGLogo.png', x = 100, y = 100, width = 100, height = 100 }) => {
   const [image] = useImage(src);
   const [isHovered, setIsHovered] = useState(false);
-
   const scale = isHovered ? 5 : 1;
-
   return (
     <Layer>
       <Image
@@ -27,6 +26,48 @@ const ImageWithHover = ({ src = './PDGLogo.png', x = 100, y = 100, width = 100, 
   );
 };
 
+const TransformShape = () => {
+  const [isSelected, setIsSelected] = useState(false)
+  const transRef = useRef(null)
+  const rectRef = useRef(null)
+
+  const setTransformShape = () => {
+    setIsSelected(!isSelected)
+    if (transRef && transRef.current)
+      transRef.current?.nodes([rectRef.current])
+  }
+
+  return (
+    <Layer draggable>
+      <Rect
+        ref={rectRef}
+        x={0}
+        y={0}
+        width={100}
+        height={100}
+        fill="yellow"
+        shadowBlur={10}
+        opacity={0.5}
+        onClick={() => setTransformShape()}
+      />
+      <Text text="<=Exit" x={20} y={40} fontSize={20} fill="white" />
+      {isSelected && (
+        <Transformer
+          flipEnabled={false}
+          ref={transRef}
+          boundBoxFunc={(oldBox, newBox) => {
+            if (Math.abs(newBox.width) < 50 || Math.abs(newBox.height) < 50) {
+              return oldBox;
+            }
+            return newBox;
+          }}
+          onDblClick={() => setIsSelected(false)}
+        />
+      )}
+    </Layer>
+  )
+}
+
 const EnterShape = () => {
   return (
     <Layer draggable>
@@ -44,7 +85,7 @@ const EnterShape = () => {
   )
 }
 
-export const InteractiveBuilding = () => {
+export const InteractiveBuilding = ({ numberShape }: { numberShape: number }) => {
   const [mammImage] = useImage('./Maam.jpg');
   const [isVisible, setIsVisible] = useState(false)
 
@@ -59,7 +100,10 @@ export const InteractiveBuilding = () => {
           onClick={e => console.log(e.target.getStage()?.pointerPos)}
         />
       </Layer>
-      <Layer >
+      {Array.from({ length: numberShape }).map((_, index) => (
+        <TransformShape key={index} />
+      ))}
+      {/* <Layer >
         <Line
           closed={true}
           fill="blue"
@@ -81,7 +125,7 @@ export const InteractiveBuilding = () => {
         </Label>
       </Layer>
       <EnterShape />
-      <ImageWithHover />
+      <ImageWithHover /> */}
     </>
   )
 }
--- app/routes/canvas.tsx
+++ app/routes/canvas.tsx
@@ -1,13 +1,19 @@
+import { useState } from "react"
 import { Stage, Layer } from "react-konva"
 import { InteractiveBuilding } from "~/components/InteractiveBuilding"
-import PDGText from "~/components/PDGText"
 
 export default function Canvas() {
+  const [numberShape, setNumberShape] = useState(0)
   return (
     <>
       <Stage width={1200} height={1200}>
-        <InteractiveBuilding />
+        <InteractiveBuilding numberShape={numberShape} />
       </Stage>
+      <button onClick={() => {
+        setNumberShape(numberShape + 1)
+      }}>
+        Create shape
+      </button>
     </>
   )
 }
